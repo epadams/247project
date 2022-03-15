d8a908d020d8348b7a5a62787471591f2121bb1f
Mahi Patel <mahimukeshpatel@gmail.com>
2022-03-15 12:03:36 -0400

    Added all instance variables

---
+++ src/RegisteredUser.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class RegisteredUser { //need to extend User
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String address;
+    private ArrayList<Passport> passports;
+    private boolean frequentFLyer;
+    private ArrayList<Flight> flightHistory;
+    private ArrayList<String> refunds;
+}
