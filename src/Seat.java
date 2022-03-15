8adf856dccc2f8b8208b0b6880a4361ff099883a
Mahi Patel <mahimukeshpatel@gmail.com>
2022-03-15 12:45:08 -0400

    added empty stubs to Seat.java

--- src/Seat.java
+++ src/Seat.java
@@ -1,4 +1,20 @@
 public class Seat {
     private SeatType type;
     private boolean vacancy;
+
+    public Seat() {
+
+    }
+
+    public Seat(SeatType type, boolean vacancy) {
+
+    }
+
+    public void setVacancy(boolean vacancy) {
+
+    }
+
+    public boolean getVacancy() {
+        return true;
+    }
 }
