1cc5e49ed7781a75012dfcb91086559c908df853
Mahi Patel <mahimukeshpatel@gmail.com>
2022-03-15 12:34:44 -0400

    added private variables to Room.java

---
+++ src/Room.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Room {
+    private int numOfBeds;
+    private boolean smoking;
+    private boolean vacancy;
+    private Date startDate;
+    private Date endDate;
+}
