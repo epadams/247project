025c9d48d334cf397d141f8b5bebd4fbe514d3ea
Mahi Patel <mahimukeshpatel@gmail.com>
2022-03-15 12:29:42 -0400

    added empty stubs to RentalCar.java

--- src/RentalCar.java
+++ src/RentalCar.java
@@ -1,5 +1,39 @@
-public class RentalCar {
+import java.util.Date;
+
+public class RentalCar extends Booking {
     private CarType type;
     private Date startDate;
     private Date endDate;
+
+    public RentalCar(){
+
+    }
+
+    public RentalCar(CarType type, Date startDate, Date endDate) {
+
+    }
+
+    public CarType getType() {
+
+    }
+
+    public Date getStartDate() {
+
+    }
+
+    public Date getEndDate() {
+
+    }
+
+    public void setType(CarType type) {
+
+    }
+
+    public void setStartDate(Date startDate) {
+
+    }
+
+    public void setEndDate(Date endDate) {
+
+    }
 }
