7e423426a59581a8446ac1c0644dd1dac2e6892c
Mahi Patel <mahimukeshpatel@gmail.com>
2022-03-15 12:41:42 -0400

    added empty stubs to Room.java

--- src/Room.java
+++ src/Room.java
@@ -6,4 +6,52 @@ public class Room {
     private boolean vacancy;
     private Date startDate;
     private Date endDate;
+
+    public Room() {
+
+    }
+
+    public Room(int numOfBeds, boolean smoking, boolean vacancy) {
+
+    }
+
+    public int getNumOfBeds() {
+        return 0;
+    }
+
+    public boolean getSmoking() {
+        return true;
+    }
+
+    public boolean getVacancy(Date startDate, Date endDate) {
+        return true;
+    }
+
+    public void setNumOfBeds(int numOfBeds){
+
+    }
+
+    public void setSmoking(boolean smoking) {
+
+    }
+
+    public void setVacancy(boolean vacancy) {
+
+    }
+
+    public Date getStartDate() {
+        return new Date();
+    }
+
+    public Date getEndDate() {
+        return new Date();
+    }
+
+    public void serStartDate(Date startDate) {
+
+    }
+
+    public void setEndDate(Date endDate) {
+
+    }
 }
