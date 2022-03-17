37ad55ad27cfa7373b276ade18ce7b90699bbb09
Mahi Patel <mahimukeshpatel@gmail.com>
2022-03-16 20:01:52 -0400

    added stubs to the Passport.java

---
+++ src/Passport.java
@@ -0,0 +1,90 @@
+package src;
+
+import java.util.Date;
+
+public class Passport {
+    private String firstName;
+    private String lastName;
+    private int passportNum;
+    private Date dateOfBirth;
+    private String placeOfBirth;
+    private Date issueDate;
+    private Date expirationDate;
+    private char sex;
+
+    public Passport() {
+
+    }
+
+    public Passport(String firstName, String lastName, int passportNum, Date dateOfBirth, String placeOfBirth, Date issueDate, Date expirationDate, char sex) {
+
+    }
+
+    public void setFirstName(String firstName) {
+
+    }
+
+    public void setLastName(String lastName) {
+
+    }
+
+    public void setPassportNum(int passportNum) {
+
+    }
+
+    public void setDateofBirth(Date dateofBirth) {
+
+    }
+
+    public void setPlaceOfBirth(String placeOfBirth) {
+
+    }
+
+    public void setIssueDate(Date issueDate) {
+
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+
+    }
+
+    public void setSex(char sex) {
+
+    }
+
+    public String getFirstName() {
+        return "";
+    }
+
+    public String getLastName() {
+        return "";
+    }
+    public int getPassportNum() {
+        return 0;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getPlaceOfBirth() {
+        return "";
+    }
+
+    public Date getIssueDate() {
+        return issueDate;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public char getSex() {
+        return 0;
+    }
+
+    public String toString() {
+        return "";
+    }
+
+}
