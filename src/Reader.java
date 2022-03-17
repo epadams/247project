74fbb79409efa376c94ff489f8ed32bd2320df67
Alex Hutton <amhutton@email.sc.edu>
2022-03-16 23:22:01 -0400

    Reader Stub Added

---
+++ src/Reader.java
@@ -0,0 +1,15 @@
+package src;
+
+import java.util.ArrayList;
+
+public class Reader {
+  private ArrayList<String> data;
+
+  public void readFromFile() {
+
+  }
+
+  public ArrayList<String> sendToSystem() {
+    return new ArrayList<String>();
+  }
+}
