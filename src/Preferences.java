a61da226792582ee07437899d2ad16f241e07994
Mahi Patel <mahimukeshpatel@gmail.com>
2022-03-16 20:09:36 -0400

    completed empty stubs on Preferences.java

---
+++ src/Preferences.java
@@ -0,0 +1,26 @@
+package src;
+
+public class Preferences {
+    private boolean medAccom;
+    private String originAirportCode;
+
+    public Preferences() {
+
+    }
+    
+    public void setMedicalAccomodation() {
+
+    }
+
+    public void setAirportOrigin() {
+
+    }
+
+    public boolean getMedicalAccomodation() {
+        return true;
+    }
+
+    public String getAirportOrigin() {
+        return "";
+    }
+}
