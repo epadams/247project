1843ed833e4f3bc748c399535f11563cb523dad3
epadams <epaadams2002@gmail.com>
2022-03-17 12:11:44 -0400

    Added Users.java

---
+++ src/Users.java
@@ -0,0 +1,26 @@
+/*
+ * TODO
+ * make it possible to add users
+ * get users
+ * save users
+ * see if a user exists
+ * etc
+ */
+
+import java.util.ArrayList;
+
+public class Users {
+  private static Users users;
+  private ArrayList<User> userList;
+
+  private Users() {
+    // add reading in users from json
+  }
+
+  public static Users getInstance () {
+    if (users == null) {
+      users = new Users();
+    }
+    return users;
+  }
+}
