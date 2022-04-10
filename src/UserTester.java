0658489168b4c7b5cf54e9f937ca42ccbb437bfe
Alex Hutton <amhutton@email.sc.edu>
2022-04-10 19:37:28 -0400

    UserTesterFinshed

--- src/UserTester.java
+++ src/UserTester.java
@@ -26,11 +26,38 @@ public class UserTester {
   }
 
   @Test
-  public void testUser_DEFConstructor() {
+  public void testUserUUID() {
     UUID uuid = UUID.randomUUID();
     User testUser = new User(uuid, "test user", "test pass", "test email", "test first", "test last", 1, "test addy",
         false);
     assertNotNull(testUser.getId(), "UUID should not be null if user is created");
+  }
 
+  @Test
+  public void testUserUserName() {
+    UUID uuid = UUID.randomUUID();
+    User testUser = new User(uuid, "test user", "test pass", "test email", "test first", "test last", 1, "test addy",
+        false);
+    assertNotNull(testUser.getUserName(), "Username should not be null if user is created");
+  }
+
+  @Test
+  public void testUserAge() {
+    UUID uuid = UUID.randomUUID();
+    User testUser = new User(uuid, "test user", "test pass", "test email", "test first", "test last", 1, "test addy",
+        false);
+    assertNotNull(testUser.getAge(), "Age should not be null if user is created");
+  }
+
+  @Test
+  public void testUser_AddPassport() {
+    UUID uuid = UUID.randomUUID();
+    User testUser = new User(uuid, "test user", "test pass", "test email", "test first", "test last", 1, "test addy",
+        false);
+    Passport testPass = new Passport();
+    Passport testPass2 = new Passport();
+    testUser.addPassport(testPass);
+    testUser.addPassport(testPass2);
+    assertNotNull(testUser.getPassports(), "Passport(s) Should be added to the User");
   }
 }
