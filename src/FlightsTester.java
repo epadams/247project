ffc8dc127026039a32e52d677c51b3dc35b59a09
mahimukeshpatel <mahimukeshpatel@gmail.com>
2022-04-10 20:03:29 -0400

    flight tester methods

--- src/FlightsTester.java
+++ src/FlightsTester.java
@@ -23,5 +23,28 @@ public class FlightsTester {
 
   }
 
-  //@Test
+  @Test
+  public void testHaveFlight_True() {
+    Flights testFlights = Flights.getInstance();
+    assertTrue(testFlights.haveFlight("CAE to ATL"), "CAE to ATL is a valid Flight");
+  }
+
+  @Test
+  public void testHaveFlight_False() {
+    Flights testFlights = Flights.getInstance();
+    assertFalse(testFlights.haveFlight("ATL to CAE"), "ATL to CAE is not a valid flight");
+  }
+
+  @Test
+  public void testArrayListNotNull() {
+    Flights testFlights = Flights.getInstance();
+    assertNotNull(testFlights.getFlights(), "flightlist should be initialized");
+  }
+
+  @Test
+  public void testAddFlight() {
+    Flights testFlights = Flights.getInstance();
+    testFlights.addFlight("Test: CAE to ATL");
+    assertTrue(testFlights.haveFlight("Test: CAE to ATL"), "Test flight no found");
+  }
 }
