a3a814c214ea6c05419dcbf128069c99fcf5f2c9
willcolumbia <columbiw@email.sc.edu>
2022-04-10 23:09:30 -0400

    testers for seat and room

--- src/RoomTester.java
+++ src/RoomTester.java
@@ -1,11 +1,13 @@
 import org.junit.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.UUID;
+
 public class RoomTester {
 
   @BeforeClass
   public static void oneTimeSetup() {
-
+    
   }
 
   @AfterClass
@@ -23,5 +25,36 @@ public class RoomTester {
 
   }
 
-  //@Test
+  @Test
+  public void testRoomUUID()
+  {
+    Room testRoom = new Room();
+    testRoom.setUUID(UUID.randomUUID());
+    assertNotNull(testRoom.getUUID(), "constructors for UUID working");
+  }
+
+  public void testRoomNumber() {
+    Room testRoom = new Room();
+    testRoom.setRoomNumber(0);
+    assertEquals(0,testRoom.getRoomNumber(), "constructors for Room Number working");
+  }
+
+  public void testNumOfBeds() {
+    Room testRoom = new Room();
+    testRoom.setNumOfBeds(3);
+    assertEquals(3,testRoom.getNumOfBeds(), "Constructors for Number of beds working");
+  }
+
+  public void testSmoking()
+  {
+    Room testRoom = new Room();
+    testRoom.setSmoking(true);
+    assertEquals(true,testRoom.getSmoking(), "Smoking constructor working");
+  }
+
+ public void testArrayListNotNull_DefConstruct()
+ {
+   Room testRoom = new Room();
+   assertNotNull(testRoom.getBookedDates(),"Booked dates is initialized");
+ }
 }
--- src/SeatTester.java
+++ src/SeatTester.java
@@ -1,6 +1,8 @@
 import org.junit.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.UUID;
+
 public class SeatTester {
 
   @BeforeClass
@@ -23,5 +25,39 @@ public class SeatTester {
 
   }
 
-  //@Test
+  @Test
+  public void testSeatUUID()
+  {
+    Seat testSeat = new Seat();
+    testSeat.setUUID(UUID.randomUUID());
+    assertNotNull(testSeat.getUUID(),"UUID working");
+  }
+
+  public void testSeatRow()
+  {
+    Seat testSeat = new Seat();
+    testSeat.setRow(2);
+    assertEquals(2,testSeat.getRow(),"Row working");
+  }
+
+  public void testSeatAisle()
+  {
+    Seat testSeat = new Seat();
+    testSeat.setAisle('a');
+    assertEquals('a', testSeat.getAisle(),"Aisle working");
+  }
+
+  public void testSeatVacancy()
+  {
+    Seat testSeat = new Seat();
+    testSeat.setVacancy(true);
+    assertEquals(true, testSeat.getVacancy(),"Vacancy working");
+  }
+
+  public void testSeatType()
+  {
+    Seat testSeat = new Seat();
+    testSeat.setType(SeatType.BUSINESS);
+    assertEquals(SeatType.BUSINESS, testSeat.getType(),"Seat type working");
+  }
 }
